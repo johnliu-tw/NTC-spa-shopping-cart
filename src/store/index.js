d185db185344e09773c4cf246b2b507410abd855
johnliu <johnliutw@hotmail.com>
2022-02-12 16:11:01 +0800

    Step 6: add `add to cart` function

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,29 @@ export default createStore({
   state: {
     serverPath: 'http://localhost:8000',
     userId: 12,
+    cart: []
+  },
+  getters:{
+    currentQuantiy(state){
+      return state.cart.reduce((productNumber, product) => parseInt(productNumber) + product.number, 0)
+    }
   },
   mutations: {
+    addCart(state, data){
+      let isNewProduct = true
+      state.cart.map(function(product){
+        if(product.id == data.product.id){
+          product.number += data.number
+          isNewProduct = false
+        }
+        return product
+      })
+
+      if(isNewProduct){
+        let newProduct = data.product
+        newProduct.number = data.number
+        state.cart.push(newProduct)
+      }
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
 })
